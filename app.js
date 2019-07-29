ef8643e6caf4dd6c6980b2bc1544967c3509142b
Joshua <mrjoshuaali@yahoo.co.uk>
2019-07-29 21:57:28 +0100

    Added socket.io to send data from nodejs to react

--- app.js
+++ app.js
@@ -7,66 +7,54 @@ const USERNAME = 'joshbetting30@yahoo.com';
 const PASSWORD = 'dUgneBc]p6>V5wF,';
 const APPKEY = 'xLFc8VQDVulIigSx';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+const io = require('socket.io')(8000);
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+io.on('connection', (client) => {
+	// Here you can start emitting events to the client 
+	console.log('connection');
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+	this.bfSession = new BetFairSession(APPKEY);
+	console.log(this.bfSession);
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+	this.bfSession.login(USERNAME, PASSWORD).then((result) => {
 
-class App {
-	constructor () {
-		this.bfSession = new BetFairSession(APPKEY);
-	}
-	start () {
-		this.bfSession.login(USERNAME, PASSWORD).then((result) => {
-
-			// An sample filter used to call BetFair method 'listMarketCatalogue'
-			// with the event type (horse racing = 7). Normally you would call
-			// 'listEvents' to get the ids of the event you want to search.
-			var exampleFilter = {
-			    filter: {
-			        "eventTypeIds": [
-			            7
-			        ],
-			        "marketCountries": [
-			            "GB"
-			        ],
-			        "marketTypeCodes": [
-			            "WIN"
-			        ],
-			        "marketStartTime": {
-			            "from": new Date().toJSON()
-			        }
-			    },
-			    "sort": "FIRST_TO_START",
-			    "maxResults": "1",
-			    "marketProjection": [
-			        "RUNNER_DESCRIPTION"
-			    ]
-			}
-			// Query to find market information for the example filter
-			this.bfSession.listMarketCatalogue(exampleFilter, function(err, res) {
-			    console.log("Response:%s\n", JSON.stringify(res.response, null, 2));
-			});
-
-			// A call to get the vendor id, which I think is required for O-auth
-			this.bfSession.getVendorClientId({filter: {}}, function(err, res) {
-				console.log(`vendor client id: ${res.result}`);
-			});
+		// An sample filter used to call BetFair method 'listMarketCatalogue'
+		// with the event type (horse racing = 7). Normally you would call
+		// 'listEvents' to get the ids of the event you want to search.
+		var exampleFilter = {
+		    filter: {
+		        "eventTypeIds": [
+		            7
+		        ],
+		        "marketCountries": [
+		            "GB"
+		        ],
+		        "marketTypeCodes": [
+		            "WIN"
+		        ],
+		        "marketStartTime": {
+		            "from": new Date().toJSON()
+		        }
+		    },
+		    "sort": "FIRST_TO_START",
+		    "maxResults": "1",
+		    "marketProjection": [
+		        "RUNNER_DESCRIPTION"
+		    ]
+		}
+		// Query to find market information for the example filter
+		this.bfSession.listMarketCatalogue(exampleFilter, function(err, res) {
+		    console.log("Response:%s\n", JSON.stringify(res.response, null, 2));
 		});
-	}
-}
 
-new App().start();
+		// A call to get the vendor id, which I think is required for O-auth
+		this.bfSession.getVendorClientId({filter: {}}, function(err, res) {
+			console.log(`vendor client id: ${res.result}`);
+		});
+
+		// Account details
+		this.bfSession.getAccountFunds({filter: {}}, function(err, res) {
+			console.log(res.result);
+		});
+	});
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Ladder from './Ladder';
-var faker = require('faker');
+import Faker from 'faker';
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://localhost:8000');
 
 const App = () => {
 	return createLadder();
 };
 
 const createLadder = () => {
-	var randomName = faker.name.findName();
+	var randomName = Faker.name.findName();
 	console.log(randomName);
 	return (
 		<div>
+		<UserItem name="josh" balance="224"/>
 		<Ladder/>
 		<Ladder/>
 		</div>
