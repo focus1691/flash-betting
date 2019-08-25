9d42011ede317de7bdaa98e70532e20f28b2b8df
Joshua <mrjoshuaali@yahoo.co.uk>
2019-08-25 22:36:53 +0100

    Database boilerplate code

---
+++ server/database.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose');
+
+const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'fcc-Mail';      // REPLACE WITH YOUR DB NAME
+
+class Database {
+  constructor() {
+    this._connect()
+  }
+  
+_connect() {
+     mongoose.connect(`mongodb://${server}/${database}`)
+       .then(() => {
+         console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error')
+       })
+  }
+}
+
+module.exports = new Database();
---
+++ server/models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('User', userSchema);
