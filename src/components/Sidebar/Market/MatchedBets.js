c60227f4e263fa1aed57699faa03e76d0cf86b9d
DragonFyZex <brandonli456@hotmail.com>
2019-08-31 23:51:39 -0700

    Fixed and Added Matched and Unmatched Bets

--- src/components/Sidebar/Market/MatchedBets.js
+++ src/components/Sidebar/Market/MatchedBets.js
@@ -1,15 +1,57 @@
 import React from 'react';
 
-const MatchedBets = () => {
+const MatchedBets = ({bets}) => {
+	if (bets[0].stake === 0.00) {
+		return null;
+	}
 	return (
 		<div>
-			<table id="menu-matched-bets">
+			<table id="menu-unmatched-bets">
 				<tbody>
-					{}
+					<React.Fragment>
+						<tr id = "menu-matched-heading">
+							<button style = {{height: "22px", width: "auto", backgroundColor: 'transparent', visibility: "collapse", pointerEvents: "none"}} />
+							<td>Odds</td>
+							<td>Stake</td>
+							<td>P/L</td>
+						</tr>
+						{   
+								bets.map(bet => (
+									<>
+										<tr id = "menu-unmatched-bet" style={{backgroundColor: bet.isBack ? "#A6D8FF" : "#FAC9D7"}}>
+										
+											<button style = {{height: "22px", width: "auto"}}>
+												{/* <img src = {require('./CancelIcon.svg')} alt="" style = {{height: "100%", width: "auto"}} /> In Progress */}
+											</button>
+											
+											<td>{bet.odds}</td>
+											<td>{bet.stake}</td>
+											<td id = "pl-style" style = {{color: bet.PL === "0.00" ? "black" : bet.PL > 0 ? "green" : "red"}}>{bet.PL}</td>
+										
+									</tr>
+									</>
+								))
+								
+								
+						}
+					</React.Fragment>
+
 				</tbody>
 			</table>
 		</div>
 	);
+}
+
+MatchedBets.defaultProps = {
+	
+	bets: [
+		{
+			odds: 0.0,
+			stake: 0.00,
+			PL: "0.00",
+			isBack: true
+		}, 
+	]
 }
 
 export default MatchedBets;
--- src/components/Sidebar/Market/UnmatchedBets.js
+++ src/components/Sidebar/Market/UnmatchedBets.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const UnmatchedBets = ({event, selection, bet}) => {
+const UnmatchedBets = ({details}) => {
+
 	return (
 		<div>
 			<table id="menu-unmatched-bets">
@@ -12,23 +13,40 @@ const UnmatchedBets = ({event, selection, bet}) => {
 							<td>Stake</td>
 							<td>P/L</td>
 						</tr>
-						<tr id = "menu-unmatched-event">
-							<td>TBP Bellewstown</td>
-						</tr>
-						<tr id = "menu-unmatched-selection">
-							<td>Shantou Bay</td>
-						</tr>
-						<tr id = "menu-unmatched-bet" style={{backgroundColor: bet.isBack ? "#A6D8FF" : "#FAC9D7"}}>
-							
-							<button style = {{height: "22px", width: "auto"}}>
-								{/* <img src = {require('./CancelIcon.svg')} alt="" style = {{height: "100%", width: "auto"}} /> In Progress */}
-							</button>
-							
-							<td>{bet.odds}</td>
-							<td>{bet.stake}</td>
-							<td id = "pl-style" style = {{color: bet.PL === "0.00" ? "black" : bet.PL > 0 ? "green" : "red"}}>{bet.PL}</td>
-							
-						</tr>
+						{   
+							details[0].event === "None" ?
+								null :
+								details.map(info => (
+									<>
+										<tr id = "menu-unmatched-event">
+											<td>{info.event}</td>
+										</tr>
+										<tr id = "menu-unmatched-selection">
+											<td>{info.selection}</td>
+										</tr>
+										{
+											info.bets.map(bet => (
+												<>
+													<tr id = "menu-unmatched-bet" style={{backgroundColor: bet.isBack ? "#A6D8FF" : "#FAC9D7"}}>
+											
+														<button style = {{height: "22px", width: "auto"}}>
+															{/* <img src = {require('./CancelIcon.svg')} alt="" style = {{height: "100%", width: "auto"}} /> In Progress */}
+														</button>
+														
+														<td>{bet.odds}</td>
+														<td>{bet.stake}</td>
+														<td id = "pl-style" style = {{color: bet.PL === "0.00" ? "black" : bet.PL > 0 ? "green" : "red"}}>{bet.PL}</td>
+													
+													</tr>
+												</>
+											))
+										}
+										
+									</>
+								))
+								
+								
+						}
 					</React.Fragment>
 
 				</tbody>
@@ -38,14 +56,21 @@ const UnmatchedBets = ({event, selection, bet}) => {
 }
 
 UnmatchedBets.defaultProps = {
-	event: "None",
-	selection: "None",
-	bet: {
-		odds: 0.0,
-		stake: 0.00,
-		PL: "0.00",
-		isBack: true
-	}
+	
+	details: [
+		{
+			event: "None",
+			selection: "None",
+			bets: [
+				{
+					odds: 0.0,
+					stake: 0.00,
+					PL: "0.00",
+					isBack: true
+				},
+			]
+		}, 
+	]
 }
 
 export default UnmatchedBets;
