207751229da7f7bbdbea42e051678969ef48a393
sh00ks <joshua@psychotechnology.com>
2019-09-28 08:17:23 +0100

    Updated stop loss checker

--- src/utils/TradingStategy/StopLoss.js
+++ src/utils/TradingStategy/StopLoss.js
@@ -2,27 +2,26 @@ const checkStopLossHit = (matchedPrice, currentPrice, side, tickOffset) => {
 
     var target = matchedPrice;
     var targetMet = false;
-  
+
     if (side === 'back') {
-      console.log(arr.indexOf(matchedPrice));
-      for (var i = arr.indexOf(matchedPrice); i < 1000; i++) {
-        target = arr[i];
-  
-        if (tickOffset-- <= 0) {
-          break;
+        for (var i = arr.indexOf(matchedPrice); i <= 1000; i++) {
+            target = arr[i];
+            if (tickOffset-- <= 0 || i === 1000) {
+                targetMet = true;
+                break;
+            }
         }
-      }
     }
     else if (side === 'lay') {
-      for (var i = arr.indexOf(matchedPrice); i > 0; i--) {
-        target = arr[i];
-  
-        if (tickOffset-- <= 0) {
-          break;
+        for (var i = arr.indexOf(currentPrice); i >= 0; i--) {
+            target = arr[i];
+            if (tickOffset-- <= 0 || i === 0) {
+                targetMet = true;
+                break;
+            }
         }
-      }
     }
     return { targetMet: targetMet, priceReached: target };
-  }
+}
 
-  export { checkStopLossHit };
+export { checkStopLossHit };
