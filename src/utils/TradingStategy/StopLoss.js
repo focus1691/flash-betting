bdb311005c9c4ca5f1fc51f8cac1d22314507fa9
Joshua <mrjoshuaali@yahoo.co.uk>
2019-09-28 20:22:01 +0100

    Updated stop loss util

--- src/utils/TradingStategy/StopLoss.js
+++ src/utils/TradingStategy/StopLoss.js
@@ -1,5 +1,17 @@
 const checkStopLossHit = (matchedPrice, currentPrice, side, tickOffset) => {
 
+    var arr =
+    Array(100).fill().map((v,i)=> (i/100 + 1.01).toFixed(2) )
+    .concat(Array(50).fill().map((v,i)=> (i/50 + 2.02).toFixed(2) ))
+    .concat(Array(20).fill().map((v,i)=> (i/20 + 3.05).toFixed(2) ))
+    .concat(Array(20).fill().map((v,i)=> (i/10 + 4.1).toFixed(1) ))
+    .concat(Array(20).fill().map((v,i)=> (i/5 + 6.2).toFixed(1) ))
+    .concat(Array(19).fill().map((v,i)=> (i/2 + 10.5).toFixed(1) ))
+    .concat(Array(11).fill().map((v,i)=> (i + 20).toFixed(0) ))
+    .concat(Array(10).fill().map((v,i)=> (i*2 + 32).toFixed(0) ))
+    .concat(Array(10).fill().map((v,i)=> (i*5 + 55).toFixed(0) ))
+    .concat(Array(90).fill().map((v,i)=> (i*10 + 110).toFixed(0) ))
+
     var target = matchedPrice;
     var targetMet = false;
 
