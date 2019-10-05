bd7dbf657491503373f15b25fc06063098f690cd
sh00ks <joshua@psychotechnology.com>
2019-10-05 22:57:32 +0100

    Sorting all sports alphabetically, and only fetching all sports once

--- src/components/Sidebar/Menu/AllSports.js
+++ src/components/Sidebar/Menu/AllSports.js
@@ -13,8 +13,18 @@ const AllSports = props => {
   useEffect(() => {
     fetch(`/api/get-all-sports`)
       .then(res => res.json())
-      .then(sports => props.onReceiveAllSports(sports));
-  });
+      .then(sports => {
+        sports.sort((a, b) => {
+          var nameA = a.eventType.name.toLowerCase(), nameB = b.eventType.name.toLowerCase()
+          if (nameA < nameB) //sort string ascending
+            return -1
+          if (nameA > nameB)
+            return 1
+          return 0 //default return value (no sorting)
+        });
+        props.onReceiveAllSports(sports);
+      });
+  }, []);
 
   useEffect(() => {
   }, [props.sports.currentSport]);
@@ -81,160 +91,160 @@ const AllSports = props => {
         <tbody>
           <List>
             {// Selecting Market
-            currentEvent !== undefined &&
-            eventMarkets !== undefined &&
-            eventMarkets.length > 0 ? (
-              <SportsClickList
-                currentItem={currentEvent.name}
-                newArray={eventMarkets}
-                currentItemName={"currentEvent"}
-                newArrayName={"eventMarkets"}
-                listSelector={"marketName"}
-                reverseClickHandler={handleClick}
-                clickHandler={market => window.open(`/dashboard?marketId=${market.marketId}`)}
-                currentItemFull={currentEvent}
-              />
-            ) : // Selecting Event
-            currentCountry !== undefined &&
-              competitionEvents !== undefined &&
-              competitionEvents.length > 0 &&
-              currentEvent === undefined ? (
-              <SportsClickList
-                currentItem={
-                  currentCompetition !== undefined
-                    ? currentCompetition.name
-                    : currentCountry
-                }
-                newArray={competitionEvents}
-                currentItemName={
-                  currentCompetition !== undefined
-                    ? "currentCompetition"
-                    : "currentCountry"
-                }
-                newArrayName={"competitionEvents"}
-                listSelector={"name"}
-                reverseClickHandler={handleClick}
-                clickHandler={data =>
-                  handleClick(
-                    data,
-                    "currentEvent",
-                    "eventMarkets",
-                    "list-markets",
-                    "",
-                    "",
-                    "",
-                    data.id,
-                    data => data.result
-                  )
-                }
-                currentItemFull={
-                  currentCompetition !== undefined
-                    ? currentCompetition
-                    : currentCountry
-                }
-              />
-            ) : // Selecting Competition
-            currentCountry !== undefined &&
-              countryCompetitions !== undefined &&
-              countryCompetitions.length > 0 &&
-              currentEvent === undefined ? (
-              <SportsClickList
-                currentItem={currentCountry}
-                newArray={countryCompetitions}
-                currentItemName={"currentCountry"}
-                newArrayName={"countryCompetitions"}
-                listSelector={"name"}
-                reverseClickHandler={handleClick}
-                clickHandler={data =>
-                  handleClick(
-                    data,
-                    "currentCompetition",
-                    "competitionEvents",
-                    "list-competition-events",
-                    currentSportId,
-                    currentCountry,
-                    data.id,
-                    "",
-                    data => data.map(item => item.event)
-                  )
-                }
-              />
-            ) : (
-              // Used for selecting sport and country
-              props.sports.sports.map(sport => {
-                // if we have a sport selected
-                if (
-                  currentSportId !== undefined &&
-                  sport.eventType.id !== currentSportId
-                ) {
-                  return null;
-                }
-
-                return (
-                  <React.Fragment>
-                    <tr>
-                      <ListItem
-                        button
-                        onClick={e =>
+              currentEvent !== undefined &&
+                eventMarkets !== undefined &&
+                eventMarkets.length > 0 ? (
+                  <SportsClickList
+                    currentItem={currentEvent.name}
+                    newArray={eventMarkets}
+                    currentItemName={"currentEvent"}
+                    newArrayName={"eventMarkets"}
+                    listSelector={"marketName"}
+                    reverseClickHandler={handleClick}
+                    clickHandler={market => window.open(`/dashboard?marketId=${market.marketId}`)}
+                    currentItemFull={currentEvent}
+                  />
+                ) : // Selecting Event
+                currentCountry !== undefined &&
+                  competitionEvents !== undefined &&
+                  competitionEvents.length > 0 &&
+                  currentEvent === undefined ? (
+                    <SportsClickList
+                      currentItem={
+                        currentCompetition !== undefined
+                          ? currentCompetition.name
+                          : currentCountry
+                      }
+                      newArray={competitionEvents}
+                      currentItemName={
+                        currentCompetition !== undefined
+                          ? "currentCompetition"
+                          : "currentCountry"
+                      }
+                      newArrayName={"competitionEvents"}
+                      listSelector={"name"}
+                      reverseClickHandler={handleClick}
+                      clickHandler={data =>
+                        handleClick(
+                          data,
+                          "currentEvent",
+                          "eventMarkets",
+                          "list-markets",
+                          "",
+                          "",
+                          "",
+                          data.id,
+                          data => data.result
+                        )
+                      }
+                      currentItemFull={
+                        currentCompetition !== undefined
+                          ? currentCompetition
+                          : currentCountry
+                      }
+                    />
+                  ) : // Selecting Competition
+                  currentCountry !== undefined &&
+                    countryCompetitions !== undefined &&
+                    countryCompetitions.length > 0 &&
+                    currentEvent === undefined ? (
+                      <SportsClickList
+                        currentItem={currentCountry}
+                        newArray={countryCompetitions}
+                        currentItemName={"currentCountry"}
+                        newArrayName={"countryCompetitions"}
+                        listSelector={"name"}
+                        reverseClickHandler={handleClick}
+                        clickHandler={data =>
                           handleClick(
-                            sport.eventType.id,
-                            "currentSportId",
-                            "sportCountries",
-                            "list-countries",
-                            sport.eventType.id
+                            data,
+                            "currentCompetition",
+                            "competitionEvents",
+                            "list-competition-events",
+                            currentSportId,
+                            currentCountry,
+                            data.id,
+                            "",
+                            data => data.map(item => item.event)
                           )
                         }
-                      >
-                        <ListItemIcon>
-                          <img
-                            src={window.location.origin + "/icons/expand.png"}
-                            alt={"Expand"}
-                          />
-                        </ListItemIcon>
-                        <ListItemText>{sport.eventType.name}</ListItemText>
-                      </ListItem>
-                      <tr>
-                        {currentSportId !== undefined &&
-                        sport.eventType.id === currentSportId ? (
-                          <SportsFilterList
-                            list={sportCountries}
-                            itemSelector={"countryCode"}
-                            clickHandler={async data => {
-                              const competitions = await handleClick(
-                                data.countryCode,
-                                "currentCountry",
-                                "countryCompetitions",
-                                "list-competitions",
-                                currentSportId,
-                                data.countryCode,
-                                "",
-                                "",
-                                data => data.map(item => item.competition)
-                              );
+                      />
+                    ) : (
+                      // Used for selecting sport and country
+                      props.sports.sports.map(sport => {
+                        // if we have a sport selected
+                        if (
+                          currentSportId !== undefined &&
+                          sport.eventType.id !== currentSportId
+                        ) {
+                          return null;
+                        }
 
-                              if (competitions.length === 0) {
-                                await handleClick(
-                                  data.countryCode,
-                                  "currentCountry",
-                                  "competitionEvents",
-                                  "list-events",
-                                  currentSportId,
-                                  data.countryCode,
-                                  "",
-                                  "",
-                                  data => data.map(item => item.event)
-                                );
-                              }
-                            }}
-                          />
-                        ) : null}
-                      </tr>
-                    </tr>
-                    <Divider />
-                  </React.Fragment>
-                );
-              })
-            )}
+                        return (
+                          <React.Fragment>
+                            <tr>
+                              <ListItem
+                                button
+                                onClick={e =>
+                                  handleClick(
+                                    sport.eventType.id,
+                                    "currentSportId",
+                                    "sportCountries",
+                                    "list-countries",
+                                    sport.eventType.id
+                                  )
+                                }
+                              >
+                                <ListItemIcon>
+                                  <img
+                                    src={window.location.origin + "/icons/expand.png"}
+                                    alt={"Expand"}
+                                  />
+                                </ListItemIcon>
+                                <ListItemText>{sport.eventType.name}</ListItemText>
+                              </ListItem>
+                              <tr>
+                                {currentSportId !== undefined &&
+                                  sport.eventType.id === currentSportId ? (
+                                    <SportsFilterList
+                                      list={sportCountries}
+                                      itemSelector={"countryCode"}
+                                      clickHandler={async data => {
+                                        const competitions = await handleClick(
+                                          data.countryCode,
+                                          "currentCountry",
+                                          "countryCompetitions",
+                                          "list-competitions",
+                                          currentSportId,
+                                          data.countryCode,
+                                          "",
+                                          "",
+                                          data => data.map(item => item.competition)
+                                        );
+
+                                        if (competitions.length === 0) {
+                                          await handleClick(
+                                            data.countryCode,
+                                            "currentCountry",
+                                            "competitionEvents",
+                                            "list-events",
+                                            currentSportId,
+                                            data.countryCode,
+                                            "",
+                                            "",
+                                            data => data.map(item => item.event)
+                                          );
+                                        }
+                                      }}
+                                    />
+                                  ) : null}
+                              </tr>
+                            </tr>
+                            <Divider />
+                          </React.Fragment>
+                        );
+                      })
+                    )}
           </List>
         </tbody>
       </table>
