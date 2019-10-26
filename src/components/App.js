b61ebab6552ddd441d17f1e8fe9eb37b99fe51e2
sh00ks <joshua@psychotechnology.com>
2019-10-26 16:06:33 +0100

    Fixed session key being sent to server infinitely

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/settings";
 import * as actions2 from "../actions/market";
