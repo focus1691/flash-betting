a0949b1c7fc9ae5c8869018ad2a32e1d80764bd3
sh00ks <joshua@psychotechnology.com>
2019-10-27 21:22:04 +0000

    HomeView top part

--- src/components/HomeView/index.js
+++ src/components/HomeView/index.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  chip: {
+    marginRight: theme.spacing(1),
+    textAlign: "right",
+  },
+  section1: {
+    margin: theme.spacing(3, 2),
+  },
+  section2: {
+    margin: theme.spacing(2),
+  },
+  section3: {
+    margin: theme.spacing(3, 1, 1),
+  },
+}));
 
 export default () => {
+  const classes = useStyles();
+
   return (
-    <div id="home-view">
-      <h1>Welcome to Trader Pro</h1>
-      <hr />
+    <div id="home-view-container" className={classes.root}>
+      <div id="home-view">
+        <Grid container alignItems="center">
+          <Grid item xs>
+            <Typography gutterBottom variant="h4">
+              Welcome to Trader Pro
+              <Chip className={classes.chip} color="primary" label="Soft" />
+            </Typography>
+          </Grid>
+        </Grid>
+        <Divider variant="middle" />
+      </div>
     </div>
   );
 };
