88bcaf1b1e4b5cab08e43a91da0a3590891d3e98
DragonFyZex <brandonli456@hotmail.com>
2019-11-17 13:29:36 -0800

    Fixed status for runners

--- src/components/GetClosedMarketStats.js
+++ src/components/GetClosedMarketStats.js
@@ -10,6 +10,7 @@ const GetClosedMarketStats = () => {
     const [cookies] = useCookies(['sessionKey', 'username']);
     const [completedOrders, setCompletedOrders] = useState([]);
     const [marketInfo, setMarketInfo] = useState({});
+    const [runners, setRunners] = useState([]);
     const marketId = getQueryVariable("marketId");
 
     const loadSession = async () => {
@@ -25,16 +26,32 @@ const GetClosedMarketStats = () => {
             // await loadSession();
             
             const marketBook = await fetch(`/api/list-market-book?marketId=${marketId}`).then(res => res.json());
-            if (!marketBook || !marketBook.response.result || marketBook.response.result.status !== "CLOSED") {
+            if (!marketBook || !marketBook.response.result || marketBook.response.result[0] || marketBook.response.result[0].status !== "CLOSED") {
                 window.location.href = window.location.origin + '/dashboard'
-            }
+            } 
+            
 
             const currentOrders = await fetch(`/api/listCurrentOrders?marketId=${marketId}`).then(res => res.json()).then(res => res.currentOrders);
             const completedOrders = currentOrders.filter(order => order.status === "EXECUTION_COMPLETE")
-            setCompletedOrders(completedOrders);
+            setCompletedOrders(completedOrders)
 
             const marketInfo = await fetch(`/api/get-market-info?marketId=${marketId}`).then(res => res.json()).then(res => res.response.result[0])
             setMarketInfo(marketInfo)
+
+            // take the runner status from the marketBook and add it to the marketInfo runners
+            if (marketBook.response && marketBook.response.result && marketBook.response.result[0].runners) {
+                const runnersStatusObject = {} // selectionId: status
+                marketBook.response.result[0].runners.map(item => {
+                    runnersStatusObject[item.selectionId] = item.status;
+                })
+
+                const runnersWithStatusArray = marketInfo.runners.map(item => {
+                    return Object.assign({}, item, {status: runnersStatusObject[item.selectionId]})
+                })
+                setRunners(runnersWithStatusArray);
+            }
+            
+
         }
         getMarketInfo();
     }, []);
@@ -43,8 +60,8 @@ const GetClosedMarketStats = () => {
         <div className={"marketstats-container"}>
             <MarketSettlement marketInfo = {marketInfo} />
             <div className={"marketstats-tables"}>
-                <ClosedMarketReport matchedBets = {completedOrders} runners = {marketInfo.runners ? marketInfo.runners : []}/>
-                <BetsPlaced matchedBets = {completedOrders} runners = {marketInfo.runners ? marketInfo.runners : []}/>
+                <ClosedMarketReport matchedBets = {completedOrders} runners = {runners ? runners : []}/>
+                <BetsPlaced matchedBets = {completedOrders} runners = {runners ? runners : []}/>
             </div>
         </div>
     )
