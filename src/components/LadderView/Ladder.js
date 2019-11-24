8328ed565cf95c612f9bef217a35e246a1df16bd
DragonFyZex <brandonli456@hotmail.com>
2019-11-24 01:28:32 -0800

    Added Background Colors to Ladder

--- src/components/LadderView/Ladder.js
+++ src/components/LadderView/Ladder.js
@@ -101,7 +101,7 @@ const Ladder = ({ id, runners, ladder, market, onPlaceOrder, onCancelOrder, onSe
     }
     const parsedVolume = {};
     if (ladder[id].trd) {
-        ladder[id].trd.map(vol => { return parsedVolume[formatPrice(vol[0])] = Math.floor(vol[1] / 100) / 10 });
+        ladder[id].trd.map(vol => { parsedVolume[formatPrice(vol[0])] = Math.floor(vol[1] / 100) / 10 });
     }
 
     const fullLadderWithProfit = {};
