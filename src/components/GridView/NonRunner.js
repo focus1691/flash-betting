5dc3f2071bcd6118fddf1f8a73ec7204121d45b4
sh00ks <joshua@psychotechnology.com>
2019-12-13 01:13:51 +0000

    Formatted non-runner name correctly in grid view

--- src/components/GridView/NonRunner.js
+++ src/components/GridView/NonRunner.js
@@ -4,13 +4,13 @@ import { DeconstructRunner } from "../../utils/Market/DeconstructRunner";
 
 export default ({sportId, nonRunners, runners, selectRunner}) => {
     return Object.keys(nonRunners).map(key => {
-        const { logo } = DeconstructRunner(runners[key], sportId);
+        const { name, number, logo } = DeconstructRunner(runners[key], sportId);
         return (
             <tr className={"grid-non-runner"}>
                 <GridDetailSuspCell
                     sportId={sportId}
-                    name={runners[key].runnerName}
-                    number={runners[key].metadata.CLOTH_NUMBER}
+                    name={name}
+                    number={number}
                     logo={logo}
                     onSelectRunner={selectRunner(runners[key])}
                 />
