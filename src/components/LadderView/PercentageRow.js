1b203c738e1a7ae5bb50299892fadf55d77d468a
sh00ks <joshua@psychotechnology.com>
2020-02-05 11:41:17 +0000

    Styled ladder header buttons with tooltip

--- src/components/LadderView/PercentageRow.js
+++ src/components/LadderView/PercentageRow.js
@@ -8,43 +8,80 @@ import { cancelOrderAction, updateOrders } from "../../actions/order";
 import { updateStopEntryList } from "../../actions/stopEntry";
 import { updateStopLossList } from "../../actions/stopLoss";
 import { updateTickOffsetList } from "../../actions/tickOffset";
-import { getLTP, getLTPDelta, getPercent, getTV } from "../../selectors/marketSelector";
+import {
+  getLTP,
+  getLTPDelta,
+  getPercent,
+  getTV
+} from "../../selectors/marketSelector";
 import { combineUnmatchedOrders } from "../../utils/Bets/CombineUnmatchedOrders";
 import { getLTPstyle } from "../../utils/ladder/DeconstructLadder";
-import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from "@material-ui/core/Tooltip";
 
-const PercentageRow = ({ ltp, tv, percent, market, ltpDelta, onChangeLadderSideLeft, ladderSideLeft, onUpdateBets, selectionId, 
-                         bets, stopLossList, tickOffsetList, stopEntryList, layList, backList, fillOrKillList,
-                         onChangeBackList, onChangeLayList, onChangeStopEntryList, onChangeTickOffsetList, onChangeStopLossList, onChangeFillOrKillList}) => {
-
+const PercentageRow = ({
+  ltp,
+  tv,
+  percent,
+  market,
+  ltpDelta,
+  onChangeLadderSideLeft,
+  ladderSideLeft,
+  onUpdateBets,
+  selectionId,
+  bets,
+  stopLossList,
+  tickOffsetList,
+  stopEntryList,
+  layList,
+  backList,
+  fillOrKillList,
+  onChangeBackList,
+  onChangeLayList,
+  onChangeStopEntryList,
+  onChangeTickOffsetList,
+  onChangeStopLossList,
+  onChangeFillOrKillList
+}) => {
   const ltpStyle = getLTPstyle(ltp, ltpDelta);
   const marketId = market.marketId;
 
   const leftSide = ladderSideLeft.toLowerCase();
 
-  const allUnmatchedSpecialBets = combineUnmatchedOrders(backList, layList, stopEntryList, tickOffsetList, stopLossList, {})[selectionId];
+  const allUnmatchedSpecialBets = combineUnmatchedOrders(
+    backList,
+    layList,
+    stopEntryList,
+    tickOffsetList,
+    stopLossList,
+    {}
+  )[selectionId];
 
   const cancelSpecialOrders = orders => {
-
     let ordersToRemove = [];
     const newBackList = Object.assign({}, backList);
     const newLayList = Object.assign({}, layList);
     const newStopEntryList = Object.assign({}, stopEntryList);
     const newTickOffsetList = Object.assign({}, tickOffsetList);
     const newStopLossList = Object.assign({}, stopLossList);
-    const newFillOrKill = Object.assign({}, fillOrKillList)
+    const newFillOrKill = Object.assign({}, fillOrKillList);
     Object.values(orders).forEach(rfs => {
       rfs.forEach(order => {
         // figure out which strategy it's using and make a new array without it
         switch (order.strategy) {
           case "Back":
-            newBackList[order.selectionId] = newBackList[order.selectionId].filter(item => item.rfs !== order.rfs);
+            newBackList[order.selectionId] = newBackList[
+              order.selectionId
+            ].filter(item => item.rfs !== order.rfs);
             break;
           case "Lay":
-            newLayList[order.selectionId] = newLayList[order.selectionId].filter(item => item.rfs !== order.rfs);
+            newLayList[order.selectionId] = newLayList[
+              order.selectionId
+            ].filter(item => item.rfs !== order.rfs);
             break;
           case "Stop Entry":
-            newStopEntryList[order.selectionId] = newStopEntryList[order.selectionId].filter(item => item.rfs !== order.rfs);
+            newStopEntryList[order.selectionId] = newStopEntryList[
+              order.selectionId
+            ].filter(item => item.rfs !== order.rfs);
             break;
           case "Tick Offset":
             delete newTickOffsetList[order.rfs];
@@ -55,31 +92,31 @@ const PercentageRow = ({ ltp, tv, percent, market, ltpDelta, onChangeLadderSideL
           case "None":
             // if we can find something that fits with the fill or kill, we can remove that (this is because we don't make another row for fill or kill)
             if (fillOrKillList[order.betId] !== undefined) {
-              ordersToRemove = ordersToRemove.concat(newFillOrKill[order.betId])
+              ordersToRemove = ordersToRemove.concat(
+                newFillOrKill[order.betId]
+              );
               delete newFillOrKill[order.betId];
             }
             break;
           default:
             break;
         }
-      
+
         ordersToRemove = ordersToRemove.concat(order);
 
         // delete from database
         try {
-          fetch('/api/remove-orders', {
+          fetch("/api/remove-orders", {
             headers: {
               Accept: "application/json",
               "Content-Type": "application/json"
             },
             method: "POST",
             body: JSON.stringify(ordersToRemove)
-          })
-        } catch (e) {
-      
-        }
-      })
-    })
+          });
+        } catch (e) {}
+      });
+    });
 
     onChangeBackList(newBackList);
     onChangeLayList(newLayList);
@@ -88,72 +125,134 @@ const PercentageRow = ({ ltp, tv, percent, market, ltpDelta, onChangeLadderSideL
     onChangeStopLossList(newStopLossList);
     onChangeFillOrKillList(newFillOrKill);
   };
-  
 
-  const cancelAllOrdersOnSide = (marketId, selectionId, side, unmatchedBets, matchedBets, specialBets, betCanceler) => async e => {
-    
-    betCanceler(specialBets)
+  const cancelAllOrdersOnSide = (
+    marketId,
+    selectionId,
+    side,
+    unmatchedBets,
+    matchedBets,
+    specialBets,
+    betCanceler
+  ) => async e => {
+    betCanceler(specialBets);
 
-    const currentOrders = await fetch(`/api/listCurrentOrders?marketId=${marketId}`).then(res => res.json()).then(res => res.currentOrders);
+    const currentOrders = await fetch(
+      `/api/listCurrentOrders?marketId=${marketId}`
+    )
+      .then(res => res.json())
+      .then(res => res.currentOrders);
 
     if (currentOrders) {
       // filter all the ones out that arent in the same selection or arent unmatched
-      const openSelectedRunnerOrders = currentOrders.filter(order => order.selectionId === parseInt(selectionId) && (order.status === "EXECUTABLE" || order.status === "PENDING") && order.side === side);
+      const openSelectedRunnerOrders = currentOrders.filter(
+        order =>
+          order.selectionId === parseInt(selectionId) &&
+          (order.status === "EXECUTABLE" || order.status === "PENDING") &&
+          order.side === side
+      );
 
       // this is basically calling 1 bet after another and returning the unmatched bets it gets from it
-      const cancelBets = await openSelectedRunnerOrders.reduce(async (previousPromise, nextOrder) => {
-        const previousCancelOrderUnmatchedBets = await previousPromise;
-        return cancelOrderAction({
-          marketId: nextOrder.marketId,
-          betId: nextOrder.betId,
-          sizeReduction: null,
-          matchedBets: matchedBets,
-          unmatchedBets: previousCancelOrderUnmatchedBets && previousCancelOrderUnmatchedBets.unmatched ? previousCancelOrderUnmatchedBets.unmatched : unmatchedBets,
-        });
-      }, Promise.resolve());
+      const cancelBets = await openSelectedRunnerOrders.reduce(
+        async (previousPromise, nextOrder) => {
+          const previousCancelOrderUnmatchedBets = await previousPromise;
+          return cancelOrderAction({
+            marketId: nextOrder.marketId,
+            betId: nextOrder.betId,
+            sizeReduction: null,
+            matchedBets: matchedBets,
+            unmatchedBets:
+              previousCancelOrderUnmatchedBets &&
+              previousCancelOrderUnmatchedBets.unmatched
+                ? previousCancelOrderUnmatchedBets.unmatched
+                : unmatchedBets
+          });
+        },
+        Promise.resolve()
+      );
 
       if (cancelBets === undefined) return;
 
       onUpdateBets({
         unmatched: cancelBets.unmatched,
         matched: cancelBets.matched
-      })
+      });
     }
-  }
-  
+  };
+
   return (
-    <div className = {"percentage-row"}>
-      <div colSpan={3} className={'th'}>{tv}</div>
-      <Tooltip title={`Cancel selection ${leftSide === 'lay' ? 'lay' : 'back'} bets`} aria-label="Cancel selections">
+    <div className={"percentage-row"}>
+      {/* Total Traded */}
+      <div colSpan={3} className={"th"}>
+        {tv}
+      </div>
+      {/* Cancel Back/Lay */}
+      <Tooltip
+        title={`Cancel selection ${leftSide === "lay" ? "lay" : "back"} bets`}
+        aria-label="Cancel selections"
+      >
         <div
-          className = {"th"}
-          style = {{cursor: 'pointer'}}
-          onClick={cancelAllOrdersOnSide(marketId, selectionId, leftSide === 'lay' ? 'LAY' : 'BACK', bets.unmatched, bets.matched, allUnmatchedSpecialBets, cancelSpecialOrders)}
+          className={"th"}
+          onClick={cancelAllOrdersOnSide(
+            marketId,
+            selectionId,
+            leftSide === "lay" ? "LAY" : "BACK",
+            bets.unmatched,
+            bets.matched,
+            allUnmatchedSpecialBets,
+            cancelSpecialOrders
+          )}
         />
       </Tooltip>
-      <div className = {"th"} style={{backgroundColor: leftSide === 'lay' ? "#FCC9D3" : "#BCE4FC"}}>
+      {/* Back/Lay */}
+      <div
+        className={"th"}
+        style={{ backgroundColor: leftSide === "lay" ? "#FCC9D3" : "#BCE4FC" }}
+      >
         {`${percent[leftSide]}%`}
       </div>
-      <div className = {"th"}
-        style={ltpStyle}
-          onClick = {onChangeLadderSideLeft(ladderSideLeft === "LAY" ? "BACK" : "LAY")}
-          >
-            {ltp[0]}
-          </div>
-      <div className = {"th"} style={{backgroundColor: leftSide === 'lay' ? "#BCE4FC" : "#FCC9D3"}}>
-        {`${percent[leftSide === "lay" ? "back" : "lay"]}%`}</div>
-      <Tooltip title={`Cancel selection ${leftSide === 'lay' ? 'back' : 'lay'} bets`} aria-label={"Cancel selections"}>
+      {/* LTP */}
+      <Tooltip title={`Swap Back/Lay Columns`} aria-label="Swap matched columns">
         <div
-          className = {"th"}
-          style = {{cursor: 'pointer'}}
-          onClick={cancelAllOrdersOnSide(marketId, selectionId, leftSide === 'lay' ? 'BACK' : 'LAY', bets.unmatched, bets.matched, allUnmatchedSpecialBets, cancelSpecialOrders)}
+          className={"th"}
+          style={ltpStyle}
+          onClick={onChangeLadderSideLeft(
+            ladderSideLeft === "LAY" ? "BACK" : "LAY"
+          )}
+        >
+          {ltp[0]}
+        </div>
+      </Tooltip>
+      {/* Lay/Back */}
+      <div
+        className={"th"}
+        style={{ backgroundColor: leftSide === "lay" ? "#BCE4FC" : "#FCC9D3" }}
+      >
+        {`${percent[leftSide === "lay" ? "back" : "lay"]}%`}
+      </div>
+      {/* Cancel Lay/Back */}
+      <Tooltip
+        title={`Cancel selection ${leftSide === "lay" ? "back" : "lay"} bets`}
+        aria-label={"Cancel selections"}
+      >
+        <div
+          className={"th"}
+          onClick={cancelAllOrdersOnSide(
+            marketId,
+            selectionId,
+            leftSide === "lay" ? "BACK" : "LAY",
+            bets.unmatched,
+            bets.matched,
+            allUnmatchedSpecialBets,
+            cancelSpecialOrders
+          )}
         />
       </Tooltip>
     </div>
   );
 };
 
-const mapStateToProps = (state, {selectionId}) => {
+const mapStateToProps = (state, { selectionId }) => {
   return {
     priceType: state.market.priceType,
     market: state.market.currentMarket,
@@ -164,10 +263,10 @@ const mapStateToProps = (state, {selectionId}) => {
     layList: state.lay.list,
     backList: state.back.list,
     fillOrKillList: state.fillOrKill.list,
-    ltp: getLTP(state.market.ladder, {selectionId}),
-    tv: getTV(state.market.ladder, {selectionId}),
-    percent: getPercent(state.market.ladder, {selectionId}),
-    ltpDelta: getLTPDelta(state.market.ladder, {selectionId}),
+    ltp: getLTP(state.market.ladder, { selectionId }),
+    tv: getTV(state.market.ladder, { selectionId }),
+    percent: getPercent(state.market.ladder, { selectionId }),
+    ltpDelta: getLTPDelta(state.market.ladder, { selectionId }),
     ladderSideLeft: state.market.ladderSideLeft
   };
 };
@@ -182,7 +281,10 @@ const mapDispatchToProps = dispatch => {
     onChangeFillOrKillList: list => dispatch(updateFillOrKillList(list)),
     onUpdateBets: bets => dispatch(updateOrders(bets)),
     onChangeLadderSideLeft: side => e => dispatch(changeLadderSideLeft(side))
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(memo(PercentageRow));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(memo(PercentageRow));
