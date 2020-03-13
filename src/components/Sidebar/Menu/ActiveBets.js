78fa80f99bb2d64d09ed8455bf2880b8059160c9
Joshua <mrjoshuaali@yahoo.co.uk>
2020-03-13 04:58:24 +0000

    Added divider to active bets

--- src/components/Sidebar/Menu/ActiveBets.js
+++ src/components/Sidebar/Menu/ActiveBets.js
@@ -1,6 +1,6 @@
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import { ListItemText, Divider } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 
 const ActiveBets = () => {
@@ -24,6 +24,7 @@ const ActiveBets = () => {
 			{
 				Array.isArray(events) && events.map(event => {
 					return (
+						<>
 						<ListItem
 							key={"active-bets-" + event.marketId}
 							button
@@ -31,6 +32,8 @@ const ActiveBets = () => {
 						>
 							<ListItemText className="active-bet-name">{event.marketName}</ListItemText>
 						</ListItem>
+						<Divider />
+						</>
 					);
 				})
 			}
