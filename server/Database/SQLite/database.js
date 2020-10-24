bffdbfbc5d39b4e47c787f08ebac9209055ef49e
Joshua <joshua@psychotechnology.com>
2020-10-24 17:57:59 +0100

    Created bet table

--- server/Database/SQLite/database.js
+++ server/Database/SQLite/database.js
@@ -4,29 +4,41 @@ class SQLiteDatabase {
   constructor() {
     this.db = new sqlite3.Database(':memory:');
     this.create();
-    this.getThem();
+    this.getBets();
   }
 
   create() {
     this.db.serialize(() => {
-      this.db.run('CREATE TABLE lorem (info TEXT)');
-
-      const stmt = this.db.prepare('INSERT INTO lorem VALUES (?)');
+      // eslint-disable-next-line max-len
+      this.db.run('CREATE TABLE bets (strategy TEXT, marketId TEXT, size INT, price TEXT, side TEXT, betId TEXT, rfs TEXT, trailing BOOLEAN, hedged BOOLEAN, assignedIsOrderMatched BOOLEAN, tickOffset INT, units TEXT, percentageTrigger INT, executionTime TEXT, timeOffset INT, seconds INT, startTime INT, targetLTP INT, stopEntryCondition TEXT)');
+      const stmt = this.db.prepare('INSERT INTO bets VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
       for (let i = 0; i < 10; i += 1) {
-        stmt.run(`Ipsum ${i}`);
+        stmt.run('BACK', '1.232323', 2, '2.02', 'BACK', '23232232', '142124124sdffs', false, false, false, null, null, null, null, null, null, null, null);
       }
       stmt.finalize();
       console.log('1');
     });
   }
+  addBet() {
 
-  getThem() {
-    this.db.serialize(() => {
-      this.db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-        console.log(`${row.id}: ${row.info}`);
-      });
-      console.log('2');
+  }
+
+  updateBet() {
+
+  }
+  
+  removeBet() {
+    
+  }
+
+  getBets() {
+    this.db.each('SELECT strategy, marketId, size, price, side, betId, rfs, trailing, hedged, assignedIsOrderMatched, tickOffset, units, percentageTrigger, executionTime, timeOffset, seconds, startTime, targetLTP, stopEntryCondition FROM bets', (err, row) => {
+      console.log(row);
     });
+    console.log('2');
+  }
+  close() {
+    this.db.close();
   }
 }
 
