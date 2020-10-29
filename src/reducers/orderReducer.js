fa168b3bc8a6f5cadc2e1a29db909278c708176f
Joshua <joshua@psychotechnology.com>
2020-10-29 18:23:22 +0000

    Fixed bet reducer wrong name in mapStateToProps

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -10,6 +10,7 @@ const reducer = (state = initialState, action) => {
     case 'PLACE_ORDER':
       return { ...state, bets: action.payload };
     case 'UPDATE_BET':
+      console.log(action.payload, state);
       return { ...state, bets: action.payload };
     default:
       return state;
