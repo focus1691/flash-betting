70d06e20d2c18547fc2dd4e6afd24e0cb8ac8bb2
Joshua <mrjoshuaali@yahoo.co.uk>
2020-10-30 01:17:51 +0000

    Created prettier config, integrated with eslint

--- src/components/Sidebar/Settings/index.jsx
+++ src/components/Sidebar/Settings/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import * as actions from '../../../actions/settings';
+import {
+  setDefaultView,
+  setTrainingBalance,
+  toggleSound,
+  toggleTools,
+  toggleUnmatchedBets,
+  toggleMatchedBets,
+  toggleGraph,
+  toggleMarketInformation,
+  setWinMarketsOnly,
+  toggleRules,
+  toggleTrainingLadderAutoCenter,
+  toggleLadderUnmatched,
+  updateStakeBtn,
+  updateLayBtn,
+  updateRightClickTicks,
+  setHorseRacingCountries,
+} from '../../../actions/settings';
 import DefaultView from './DefaultView';
 import TrainingBank from './TrainingBank';
 import Sounds from './Sounds';
@@ -40,10 +57,38 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Settings = ({
-  premiumMember, defaultView, onToggleDefaultView, onReceiveTrainingBalance, sounds, onToggleSounds, tools, onToggleTools,
-  unmatchedBets, onToggleUnmatchedBets, matchedBets, onToggleMatchedBets, graphs, onToggleGraph, marketInfo, onToggleMarketInformation,
-  winMarketsOnly, onUpdateWinMarketsOnly, rules, onToggleRules, trainingLadderAutoCenter, toggleTrainingLadderAutoCenter, ladderUnmatched,
-  onToggleLadderUnmatched, stakeBtns, onUpdateStakeBtn, layBtns, onUpdateLayBtn, rightClickTicks, onUpdateRightClickTicks, horseRaces, onUpdateHorseRaces,
+  premiumMember,
+  defaultView,
+  setDefaultView,
+  setTrainingBalance,
+  sounds,
+  onToggleSounds,
+  tools,
+  onToggleTools,
+  unmatchedBets,
+  onToggleUnmatchedBets,
+  matchedBets,
+  onToggleMatchedBets,
+  graphs,
+  onToggleGraph,
+  marketInfo,
+  onToggleMarketInformation,
+  winMarketsOnly,
+  onUpdateWinMarketsOnly,
+  rules,
+  onToggleRules,
+  trainingLadderAutoCenter,
+  toggleTrainingLadderAutoCenter,
+  ladderUnmatched,
+  onToggleLadderUnmatched,
+  stakeBtns,
+  onUpdateStakeBtn,
+  layBtns,
+  onUpdateLayBtn,
+  rightClickTicks,
+  onUpdateRightClickTicks,
+  horseRaces,
+  onUpdateHorseRaces,
 }) => {
   const classes = useStyles();
 
@@ -60,116 +105,37 @@ const Settings = ({
 
   return (
     <div>
-      <DefaultView
-        defaultView={defaultView}
-        toggleDefaultView={onToggleDefaultView}
-        saveSetting={saveSetting}
-        premiumMember={premiumMember}
-        classes={classes}
-      />
+      <DefaultView defaultView={defaultView} toggleDefaultView={setDefaultView} saveSetting={saveSetting} premiumMember={premiumMember} classes={classes} />
 
-      <TrainingBank
-        receiveTrainingBalance={(balance) => onReceiveTrainingBalance(balance)}
-        styles={classes}
-      />
+      <TrainingBank receiveTrainingBalance={(balance) => setTrainingBalance(balance)} styles={classes} />
 
-      <Sounds
-        sounds={sounds}
-        toggleSounds={onToggleSounds}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <Sounds sounds={sounds} toggleSounds={onToggleSounds} saveSetting={saveSetting} classes={classes} />
 
-      <Tools
-        tools={tools}
-        toggleTools={onToggleTools}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <Tools tools={tools} toggleTools={onToggleTools} saveSetting={saveSetting} classes={classes} />
 
-      <UnmatchedBets
-        unmatchedBets={unmatchedBets}
-        toggleUnmatchedBets={onToggleUnmatchedBets}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <UnmatchedBets unmatchedBets={unmatchedBets} toggleUnmatchedBets={onToggleUnmatchedBets} saveSetting={saveSetting} classes={classes} />
 
-      <MatchedBets
-        matchedBets={matchedBets}
-        toggleMatchedBets={onToggleMatchedBets}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <MatchedBets matchedBets={matchedBets} toggleMatchedBets={onToggleMatchedBets} saveSetting={saveSetting} classes={classes} />
 
-      <Graphs
-        graphs={graphs}
-        toggleGraph={onToggleGraph}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <Graphs graphs={graphs} toggleGraph={onToggleGraph} saveSetting={saveSetting} classes={classes} />
 
-      <MarketInformation
-        marketInfo={marketInfo}
-        toggleMarketInformation={onToggleMarketInformation}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <MarketInformation marketInfo={marketInfo} toggleMarketInformation={onToggleMarketInformation} saveSetting={saveSetting} classes={classes} />
 
-      <WinMarkets
-        winMarketsOnly={winMarketsOnly}
-        updateWinMarketsOnly={onUpdateWinMarketsOnly}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <WinMarkets winMarketsOnly={winMarketsOnly} updateWinMarketsOnly={onUpdateWinMarketsOnly} saveSetting={saveSetting} classes={classes} />
 
-      <Rules
-        rules={rules}
-        toggleRules={onToggleRules}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <Rules rules={rules} toggleRules={onToggleRules} saveSetting={saveSetting} classes={classes} />
 
-      <LadderAutoCenter
-        trainingLadderAutoCenter={trainingLadderAutoCenter}
-        toggleTrainingLadderAutoCenter={toggleTrainingLadderAutoCenter}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <LadderAutoCenter trainingLadderAutoCenter={trainingLadderAutoCenter} toggleTrainingLadderAutoCenter={toggleTrainingLadderAutoCenter} saveSetting={saveSetting} classes={classes} />
 
-      <LadderUnmatchedCol
-        ladderUnmatched={ladderUnmatched}
-        toggleLadderUnmatched={onToggleLadderUnmatched}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <LadderUnmatchedCol ladderUnmatched={ladderUnmatched} toggleLadderUnmatched={onToggleLadderUnmatched} saveSetting={saveSetting} classes={classes} />
 
-      <StakeButtons
-        stakeBtns={stakeBtns}
-        updateStakeBtn={onUpdateStakeBtn}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <StakeButtons stakeBtns={stakeBtns} updateStakeBtn={onUpdateStakeBtn} saveSetting={saveSetting} classes={classes} />
 
-      <LayButtons
-        layBtns={layBtns}
-        updateLayBtn={onUpdateLayBtn}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <LayButtons layBtns={layBtns} updateLayBtn={onUpdateLayBtn} saveSetting={saveSetting} classes={classes} />
 
-      <RightClickTicks
-        rightClickTicks={rightClickTicks}
-        updateRightClickTicks={onUpdateRightClickTicks}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <RightClickTicks rightClickTicks={rightClickTicks} updateRightClickTicks={onUpdateRightClickTicks} saveSetting={saveSetting} classes={classes} />
 
-      <HorseRacing
-        horseRaces={horseRaces}
-        toggleHorseRaces={onUpdateHorseRaces}
-        saveSetting={saveSetting}
-        classes={classes}
-      />
+      <HorseRacing horseRaces={horseRaces} toggleHorseRaces={onUpdateHorseRaces} saveSetting={saveSetting} classes={classes} />
     </div>
   );
 };
@@ -195,23 +161,23 @@ const mapStateToProps = (state) => ({
   horseRaces: state.settings.horseRaces,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onToggleDefaultView: (view) => dispatch(actions.setDefaultView(view)),
-  onReceiveTrainingBalance: (balance) => dispatch(actions.setTrainingBalance(balance)),
-  onToggleSounds: (isSelected) => dispatch(actions.toggleSound(isSelected)),
-  onToggleTools: (settings) => dispatch(actions.toggleTools(settings)),
-  onToggleUnmatchedBets: (settings) => dispatch(actions.toggleUnmatchedBets(settings)),
-  onToggleMatchedBets: (settings) => dispatch(actions.toggleMatchedBets(settings)),
-  onToggleGraph: (settings) => dispatch(actions.toggleGraph(settings)),
-  onToggleMarketInformation: (settings) => dispatch(actions.toggleMarketInformation(settings)),
-  onUpdateWinMarketsOnly: (isChecked) => dispatch(actions.setWinMarketsOnly(isChecked)),
-  onToggleRules: (settings) => dispatch(actions.toggleRules(settings)),
-  onToggleTrainingLadderAutoCenter: (settings) => dispatch(actions.toggleTrainingLadderAutoCenter(settings)),
-  onToggleLadderUnmatched: (unmatchedColumn) => dispatch(actions.toggleLadderUnmatched(unmatchedColumn)),
-  onUpdateStakeBtn: (data) => dispatch(actions.updateStakeBtn(data)),
-  onUpdateLayBtn: (data) => dispatch(actions.updateLayBtn(data)),
-  onUpdateRightClickTicks: (ticks) => dispatch(actions.updateRightClickTicks(ticks)),
-  onUpdateHorseRaces: (horseRaces) => dispatch(actions.setHorseRacingCountries(horseRaces)),
-});
+const mapDispatchToProps = {
+  setDefaultView,
+  setTrainingBalance,
+  toggleSound,
+  toggleTools,
+  toggleUnmatchedBets,
+  toggleMatchedBets,
+  toggleGraph,
+  toggleMarketInformation,
+  setWinMarketsOnly,
+  toggleRules,
+  toggleTrainingLadderAutoCenter,
+  toggleLadderUnmatched,
+  updateStakeBtn,
+  updateLayBtn,
+  updateRightClickTicks,
+  setHorseRacingCountries,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings);
