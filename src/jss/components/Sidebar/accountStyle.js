bf4b85d1537b6061bfc480fcd46d45387c856916
Joshua <mrjoshuaali@yahoo.co.uk>
2020-12-22 04:20:44 +0000

    Updated sidebar

--- src/jss/components/Sidebar/accountStyle.js
+++ src/jss/components/Sidebar/accountStyle.js
@@ -1,11 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   header: {
-    backgroundColor: '#000000',
-    backgroundImage: 'linear-gradient(315deg, #000000 0%, #414141 74%)',
-    padding: '5px',
-    fontWeight: 'bold',
+    paddingBottom: theme.spacing(4),
+    borderRadius: theme.spacing(0, 0, 4, 4),
+    filter: 'drop-shadow(0px 6px 5px #333131)',
+    backgroundColor: '#3a3b3c',
     '& p': {
       color: '#fff',
     },
