9c586515367f3bbd70fbe3ae06fdf359a70315d9
Joshua <joshua@psychotechnology.com>
2021-01-27 22:06:19 +0000

    Removed old homepage content

--- src/components/HomeView/Header.jsx
+++ src/components/HomeView/Header.jsx
@@ -1,20 +1,14 @@
+import React from 'react';
+//* @material-ui core
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import React from 'react';
 
 export default ({ username, premiumMember, classes }) => (
   <Grid container alignItems="center">
-    <Grid item xs>
-      <Typography className={classes.title} gutterBottom variant="h4">
-        Welcome to Flash Betting
-      </Typography>
-    </Grid>
     <Grid item lg container justify="flex-end">
       <Chip className={classes.username} color="primary" label={`${username} | Support ID 24442`} />
       <Chip
         className={classes.subscription}
-        color="primary"
         label={`Subscription: ${premiumMember ? 'Active' : 'Expired'}`}
       />
     </Grid>
--- src/components/HomeView/index.jsx
+++ src/components/HomeView/index.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
 import { connect } from 'react-redux';
-//* @material-ui core
-import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { openPremiumDialog, setSelectedPremium } from '../../actions/settings';
+//* Custom Components
 import Header from './Header';
 import SubscriptionContainer from './SubscriptionContainer';
+//* Actions
+import { openPremiumDialog, setSelectedPremium } from '../../actions/settings';
+//* JSS
 import useStyles from '../../jss/components/HomeView/homeViewStyle';
 
 const cookies = new Cookies();
@@ -32,61 +31,32 @@ const HomeView = ({ premiumMember, openPremiumDialog, setSelectedPremium }) => {
 
   return (
     <div className={classes.root}>
-      <div className={classes.homeView}>
-        <Header username={cookies.get('username')} premiumMember={premiumMember} classes={classes} />
-        <Divider className={classes.divider} />
-        <Grid container alignItems="center">
-          <Typography className={classes.subheading} variant="h5">
-            Flash Betting Membership is
-            {' '}
-            <span style={{ color: data.color }}>{data.subscribed}</span>
-          </Typography>
-        </Grid>
-        <Grid container alignItems="center">
-          <Typography className={classes.premiumStatus} gutterBottom variant="h6">
-            Your Flash Betting Membership is
-            {' '}
-            <span>{data.subscribed}</span>
-          </Typography>
-          <Typography variant="body1">{data.information}</Typography>
-        </Grid>
-        <Grid container alignItems="center">
-          <Typography className={classes.textMargin} gutterBottom variant="body1">
-            Contact us on
-            {' '}
-            <a href="mailto:joshbetting30@yahoo.com">joshbetting30@yahoo.com</a>
-            {' '}
-            if you experience any
-            issues when using Flash Betting.
-          </Typography>
-        </Grid>
-        <Divider className={classes.divider} />
-        <div className={classes.subscriptionList}>
-          {premiumMember ? null : (
-            <>
-              <SubscriptionContainer
-                openPremiumDialog={openPremiumDialog}
-                setSelectedPremium={setSelectedPremium}
-                classes={classes}
-              />
-              <SubscriptionContainer
-                plan="Biannually"
-                price={49.99}
-                color="green"
-                openPremiumDialog={openPremiumDialog}
-                setSelectedPremium={setSelectedPremium}
-                classes={classes}
-              />
-              <SubscriptionContainer
-                plan="Annually"
-                price={99.99}
-                openPremiumDialog={openPremiumDialog}
-                setSelectedPremium={setSelectedPremium}
-                classes={classes}
-              />
-            </>
-          )}
-        </div>
+      <Header username={cookies.get('username')} premiumMember={premiumMember} classes={classes} />
+      <div className={classes.subscriptionList}>
+        {premiumMember ? null : (
+          <>
+            <SubscriptionContainer
+              openPremiumDialog={openPremiumDialog}
+              setSelectedPremium={setSelectedPremium}
+              classes={classes}
+            />
+            <SubscriptionContainer
+              plan="Biannually"
+              price={49.99}
+              color="green"
+              openPremiumDialog={openPremiumDialog}
+              setSelectedPremium={setSelectedPremium}
+              classes={classes}
+            />
+            <SubscriptionContainer
+              plan="Annually"
+              price={99.99}
+              openPremiumDialog={openPremiumDialog}
+              setSelectedPremium={setSelectedPremium}
+              classes={classes}
+            />
+          </>
+        )}
       </div>
     </div>
   );
