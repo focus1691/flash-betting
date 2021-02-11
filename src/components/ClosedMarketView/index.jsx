cf42be0bd46e97f5ac06e96232a823e7ae6eb961
Joshua <mrjoshuaali@yahoo.co.uk>
2021-02-11 02:21:00 +0000

    fixed closed market report for missing values

--- src/components/ClosedMarketView/index.jsx
+++ src/components/ClosedMarketView/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 //* Actions
 import { setPremiumStatus } from '../../actions/settings';
@@ -14,10 +15,10 @@ import { isPremiumActive } from '../../utils/DateCalculator';
 //* JSS
 import useStyles from '../../jss/components/ClosedMarketView';
 
-const ClosedMarketView = ({ setPremiumStatus }) => {
+const ClosedMarketView = ({ runners, setPremiumStatus }) => {
   const classes = useStyles();
   const [completedOrders, setCompletedOrders] = useState([]);
-  const [runners, setRunners] = useState([]);
+  const [runnerDetails, setRunnerDetails] = useState([]);
   const [marketInfo, setMarketInfo] = useState({});
   const marketId = getQueryVariable('marketId');
 
@@ -31,17 +32,35 @@ const ClosedMarketView = ({ setPremiumStatus }) => {
       const marketBook = await fetchData(`/api/list-market-book?marketId=${marketId}`);
       const marketCatalogue = await fetchData(`api/get-market-info?marketId=${marketId}`);
 
-      if (marketBook.length > 0 && marketCatalogue.length > 0) {
-        // Store the race status of each runner
-        const runnerStatus = {};
-        marketBook[0].runners.forEach(({ selectionId, status }) => {
-          runnerStatus[selectionId] = status;
-        });
+      if (marketBook.length > 0) {
+        //* runners will be empty if route is accessed directly for selected marketId
+        //* Otherwise it will remain in redux from the market open
+        if (_.isEmpty(runners)) {
+          //! marketCatalogue runner {} contains the runner name
+          if (marketCatalogue.length > 0) {
+            // Store the race status of each runner
+            const runnerStatus = {};
+            marketBook[0].runners.forEach(({ selectionId, status }) => {
+              runnerStatus[selectionId] = status;
+            });
 
-        // Conflate the id/name with the status
-        const runners = marketCatalogue[0].runners.map(({ selectionId, runnerName }) => ({ selectionId, runnerName, status: runnerStatus[selectionId] })).filter(({ status }) => status !== 'REMOVED');
-        setRunners(runners);
+            // Conflate the selectionId / runnerName with the race status
+            const runnerDetails = marketCatalogue[0].runners.map(({ selectionId, runnerName }) => ({ selectionId, runnerName, status: runnerStatus[selectionId] })).filter(({ status }) => status !== 'REMOVED');
+            setRunnerDetails(runnerDetails);
 
+          } else {
+            //! Runner name not retrievable in redux, or in the marketCatalogue, proceed without it
+            const runnerDetails = marketBook[0].runners.map(({ selectionId, status }) => ({ selectionId, status, runnerName: '' })).filter(({ status }) => status !== 'REMOVED');
+            setRunnerDetails(runnerDetails);
+          }        
+        } else {
+          //! Runner name is retrievable through in the runners {} from redux
+          const runnerDetails = marketBook[0].runners.map(({ selectionId, status }) => ({ selectionId, status, runnerName: runners[selectionId].runnerName })).filter(({ status }) => status !== 'REMOVED');
+          setRunnerDetails(runnerDetails);
+        }
+      }
+
+      if (marketCatalogue.length > 0) {
         setMarketInfo(marketCatalogue[0]);
       }
 
@@ -56,15 +75,19 @@ const ClosedMarketView = ({ setPremiumStatus }) => {
     <div className={classes.container}>
       <MarketSettlement marketInfo={marketInfo} />
       <div className={classes.tables}>
-        <MarketReport matchedBets={completedOrders} runners={runners} />
-        <BetsPlaced matchedBets={completedOrders} runners={runners} />
+        <MarketReport matchedBets={completedOrders} runners={runnerDetails} />
+        <BetsPlaced matchedBets={completedOrders} runners={runnerDetails} />
       </div>
     </div>
   );
 };
 
+const mapStateToProps = (state) => ({
+  runners: state.market.runners,
+});
+
 const mapDispatchToProps = {
   setPremiumStatus,
 };
 
-export default connect(null, mapDispatchToProps)(ClosedMarketView);
+export default connect(mapStateToProps, mapDispatchToProps)(ClosedMarketView);
