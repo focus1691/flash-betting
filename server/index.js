7502625016b4ef754a1431c29ed384d5753f9b79
Joshua <mrjoshuaali@yahoo.co.uk>
2021-02-17 22:51:54 +0000

    shortened process close handlers code

--- server/index.js
+++ server/index.js
@@ -717,42 +717,17 @@ const exitHandler = (options, exitCode) => {
 };
 
 // App is closing
-process.on(
-  'exit',
-  exitHandler.bind(null, {
-    cleanup: true,
-  }),
-);
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
 
 // Catches ctrl+c event
-process.on(
-  'SIGINT',
-  exitHandler.bind(null, {
-    exit: true,
-  }),
-);
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
 // Catches "kill pid" (for example: nodemon restart)
-process.on(
-  'SIGUSR1',
-  exitHandler.bind(null, {
-    exit: true,
-  }),
-);
-process.on(
-  'SIGUSR2',
-  exitHandler.bind(null, {
-    exit: true,
-  }),
-);
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 
 // Catches uncaught exceptions
-process.on(
-  'uncaughtException',
-  exitHandler.bind(null, {
-    exit: true,
-  }),
-);
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 
 io.on('connection', async (client) => {
   const accessToken = await Database.getToken(betfair.email);
