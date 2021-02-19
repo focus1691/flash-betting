427f5d2a3bfafad73230b3fec23bbff50d6bd4dd
Joshua <joshua@psychotechnology.com>
2021-02-19 18:24:16 +0000

    Updated validator urls

--- server/utils/Validator.js
+++ server/utils/Validator.js
@@ -1,7 +1,7 @@
 function isAuthURL(url) {
-  return url === '/api/login' || url === '/api/logout';
+  return url.includes('login') || url.includes('logout') || url.includes('get-subscription-status') || url.includes('get-vendor-client-id') || url.includes('keep-alive');
 }
 
 module.exports = {
   isAuthURL,
-}
+};
