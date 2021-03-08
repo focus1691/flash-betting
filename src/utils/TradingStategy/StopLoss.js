01ac79353ecd8032623348781a78e2b4df2d1cb2
Joshua <mrjoshuaali@yahoo.co.uk>
2021-03-08 00:23:23 +0000

    Updated stop position function

--- src/utils/TradingStategy/StopLoss.js
+++ src/utils/TradingStategy/StopLoss.js
@@ -2,9 +2,27 @@
 import { calcPercentDifference } from '../Bets/BettingCalculations';
 import { ALL_PRICES } from '../ladder/CreateFullLadder';
 
-export const calcStopLossPrice = (price, ticks, side) => {
+export const calcStopLossPrice = (price, ticks, side, percent = false) => {
   price = Number(price);
-  ticks = side == 'BACK' ? +ticks : -ticks; // Add for back, subtract for lay
+  side = side.toUpperCase();
+
+  if (percent) {
+    percent = Math.min(Math.max(0, ticks), 1000);
+    const priceFromPercent = ((price * percent) / 100);
+    const target = (side === 'BACK') ? price + priceFromPercent : price - priceFromPercent;
+
+    return ALL_PRICES.reduce((a, b) => {
+      const aDiff = Math.abs(a - target);
+      const bDiff = Math.abs(b - target);
+
+      if (aDiff == bDiff) {
+        return a > b ? a : b;
+      }
+      return bDiff < aDiff ? b : a;
+    });
+  }
+
+  ticks = side === 'BACK' ? +ticks : -ticks; // Add for back, subtract for lay
   let indexToStop = ALL_PRICES.indexOf(price) + ticks; // Price index
   indexToStop = Math.min(Math.max(0, indexToStop), ALL_PRICES.length - 1); // Must be between 1.01 - 1000
   return ALL_PRICES[indexToStop];
--- src/utils/TradingStategy/TickOffset.js
+++ src/utils/TradingStategy/TickOffset.js
@@ -2,11 +2,12 @@ import { ALL_PRICES } from '../ladder/CreateFullLadder';
 
 export const calcTickOffsetPrice = (price, side, ticks, percent = false) => {
   price = Number(price);
+  side = side.toUpperCase();
 
   if (percent) {
     percent = Math.min(Math.max(0, ticks), 1000);
     const priceFromPercent = ((price * percent) / 100);
-    const target = (side == 'BACK') ? price - priceFromPercent : price + priceFromPercent;
+    const target = (side === 'BACK') ? price - priceFromPercent : price + priceFromPercent;
 
     return ALL_PRICES.reduce((a, b) => {
       const aDiff = Math.abs(a - target);
@@ -19,13 +20,10 @@ export const calcTickOffsetPrice = (price, side, ticks, percent = false) => {
     });
   }
 
-  const priceIndex = ALL_PRICES.indexOf(price);
-  if (priceIndex === -1) return null;
-
-  if (side == 'BACK') return ALL_PRICES[Math.max(0, priceIndex - ticks)];
-  if (side == 'LAY') return ALL_PRICES[Math.min(1000, priceIndex + ticks)];
-
-  return null;
+  ticks = side === 'BACK' ? -ticks : +ticks; // Add for back, subtract for lay
+  let indexToStop = ALL_PRICES.indexOf(price) + ticks; // Price index
+  indexToStop = Math.min(Math.max(0, indexToStop), ALL_PRICES.length - 1); // Must be between 1.01 - 1000
+  return ALL_PRICES[indexToStop];
 };
 
 export const checkTickOffsetTrigger = (tos, sizeMatched) => tos && sizeMatched / tos.size >= tos.percentageTrigger / 100;
