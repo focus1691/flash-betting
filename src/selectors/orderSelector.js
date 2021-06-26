325b1e61c78bbeec2b00d20e6054896d55410df1
Joshua <joshua@psychotechnology.com>
2021-06-26 14:07:15 +0100

    Organised selectors

--- src/selectors/orderSelector.js
+++ src/selectors/orderSelector.js
@@ -1,23 +1,18 @@
 import { createSelector } from 'reselect';
 
 export const getUnmatchedBets = createSelector((state) => state, (state) => state.unmatched);
-
 export const getMatchedBets = createSelector((state) => state, (state) => state.matched);
 
 const getMarketMatchedBetsSelector = (state, { marketId }) => ({ bets: state.matched, marketId });
-
 export const getMarketMatchedBets = createSelector(getMarketMatchedBetsSelector, ({ bets, marketId }) => Object.values(bets).filter((bet) => (parseInt(bet.marketId) === parseInt(marketId))));
 
 const getMarketUnmatchedBetsSelector = (state, { marketId }) => ({ bets: state.unmatched, marketId });
-
 export const getMarketUnmatchedBets = createSelector(getMarketUnmatchedBetsSelector, ({ bets, marketId }) => Object.values(bets).filter((bet) => bet.marketId == marketId));
 
 const getSelectionUnmatchedBetsSelector = (state, { selectionId }) => ({ bets: state.unmatched, selectionId });
-
 export const getSelectionUnmatchedBets = createSelector(getSelectionUnmatchedBetsSelector, ({ bets, selectionId }) => Object.values(bets).filter((bet) => bet.selectionId == selectionId));
 
 const getSelectionMatchedBetsSelector = (state, { selectionId }) => ({ bets: state.matched, selectionId });
-
 export const getSelectionMatchedBets = createSelector(getSelectionMatchedBetsSelector, ({ bets, selectionId }) => Object.values(bets).filter((bet) => (parseInt(bet.selectionId) === parseInt(selectionId))));
 
 const getUnmatchedBetsOnRowSelector = (state, { selectionId, price, side }) => ({
