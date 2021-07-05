9c2e882934c691438068275c30c140831bcba60b
Joshua <joshua@psychotechnology.com>
2021-07-05 21:57:57 +0100

    Added test for spinner

--- src/components/App/Spinner.jsx
+++ src/components/App/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Loader from 'react-loader-spinner';
 
 export default () => (
-  <div id="spinner">
+  <div id="spinner" data-testid="spinner">
     <Loader type="ThreeDots" color="#007aaf" height={100} width={100} />
   </div>
 );
--- src/tests/components/app.test.js
+++ src/tests/components/app.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { queryByTestId, render, screen } from '@testing-library/react';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import socketIOClient from 'socket.io-client';
 import SocketMock from 'socket.io-mock';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+//* Components
+import App from '../../components/App';
 //* Contexts
 import SocketContext from '../../contexts/SocketContext';
 //* Reducers
 import reducers from '../../redux/reducers';
+import combineMiddleWares from '../../redux/CombineMiddlewares';
+import rootSaga from '../../redux/saga';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers(reducers);
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [...combineMiddleWares(), sagaMiddleware];
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+sagaMiddleware.run(rootSaga);
 
 const server = setupServer(
   rest.get('/greeting', (req, res, ctx) => {
@@ -16,8 +32,13 @@ const server = setupServer(
 );
 
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+afterEach(() => {
+  server.resetHandlers();
+  jest.resetAllMocks();
+});
+afterAll(() => {
+  server.close();
+});
 
 describe('App startup loading', () => {
   it('Sockets should be able to talk to each other without a server', async () => {
@@ -27,5 +48,17 @@ describe('App startup loading', () => {
       expect(message).toEqual('Hello World!');
     });
     socket.socketClient.emit('message', 'Hello World!');
+
+    const wrapper = render(
+      <Provider store={store}>
+        <SocketContext.Provider value={socket}>
+          <App />
+        </SocketContext.Provider>
+      </Provider>,
+    );
+
+    const spinner = screen.getByTestId('spinner');
+    
+    expect(spinner).toBeTruthy();
   });
 });
