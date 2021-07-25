45c00e56819b6105cbbe918ba94069b15916bdbd
Joshua <mrjoshuaali@yahoo.co.uk>
2021-07-26 00:16:03 +0100

    updated stop loss/stop entry sagas

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -1,13 +1,10 @@
 import { all } from 'redux-saga/effects';
 import { watchMarketUpdates } from './marketUpdateSagas';
-import { watchStopLossBet, watchStopLossTicks } from './stopLossSagas';
 import { watchTickOffsetBet } from './tickOffsetSagas';
 
 export default function* rootSaga() {
   yield all([
     watchMarketUpdates(),
-    watchStopLossBet(),
-    watchStopLossTicks(),
     watchTickOffsetBet(),
   ]);
 }
