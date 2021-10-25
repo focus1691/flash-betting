2c0267ce70f899049ec3ed33cc79d0e24fb6858d
Joshua <joshua@coinrule.com>
2021-10-25 21:29:52 +0100

    Cleaned up electron

--- public/electron.js
+++ public/electron.js
@@ -5,11 +5,10 @@ require('dotenv').config();
 const electron = require('electron');
 
 const electronApp = electron.app;
-electronApp.commandLine.appendSwitch('high-dpi-support', 1)
-electronApp.commandLine.appendSwitch('force-device-scale-factor', 1)
+electronApp.commandLine.appendSwitch('high-dpi-support', 1);
+electronApp.commandLine.appendSwitch('force-device-scale-factor', 1);
 
 const { BrowserWindow } = electron;
-// const { BrowserWindow } = remote;
 let mainWindow;
 
 require('../server');
