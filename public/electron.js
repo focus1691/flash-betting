16f6ec5b8464d641c0b8edb2f04a4ef7476451eb
Joshua <joshua@coinrule.com>
2021-11-11 18:55:51 +0000

    Only one instance of electron running at a time

--- public/electron.js
+++ public/electron.js
@@ -4,22 +4,50 @@ require('dotenv').config();
 //* Electron
 const electron = require('electron');
 
-const electronApp = electron.app;
-electronApp.commandLine.appendSwitch('high-dpi-support', 1);
-electronApp.commandLine.appendSwitch('force-device-scale-factor', 1);
+const { app } = electron;
+app.commandLine.appendSwitch('high-dpi-support', 1);
+app.commandLine.appendSwitch('force-device-scale-factor', 1);
 
 const { BrowserWindow } = electron;
 let mainWindow;
 
 require('../server');
 
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+
+  app.on('ready', createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });  
+}
+
 function createWindow() {
   const screenSize = electron.screen.getPrimaryDisplay().size;
   mainWindow = new BrowserWindow({
     width: screenSize.width,
     height: screenSize.height,
-    minWidth: screenSize.width * 0.75,
-    minHeight: screenSize.height * 0.75,
+    minWidth: screenSize.width * 0.80,
+    minHeight: screenSize.height * 0.80,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -37,17 +65,3 @@ function createWindow() {
     mainWindow = null;
   });
 }
-
-electronApp.on('ready', createWindow);
-
-electronApp.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    electronApp.quit();
-  }
-});
-
-electronApp.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
