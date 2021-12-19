dfe39f4a383583f7f38a9acdaef4ee930b7e7b16
Joshua <mrjoshuaali@yahoo.co.uk>
2021-12-19 04:45:24 +0000

    Sending updates min every 1 second

--- server/betfair/exchange-stream.js
+++ server/betfair/exchange-stream.js
@@ -1,4 +1,5 @@
 const { StringDecoder } = require('string_decoder');
+const { clearInterval } = require('timers');
 
 const decoder = new StringDecoder('utf8');
 
@@ -12,8 +13,10 @@ class BetFairStreamAPI {
     this.client = null;
     this.socket = socket;
     this.chunks = [];
+    this.updates = [];
     this.subscriptions = [];
     this.isTestDisconnected = false;
+    this.messageSender = null;
   }
 
   authenticate(accessToken) {
@@ -33,9 +36,12 @@ class BetFairStreamAPI {
       }
       console.log(authParams);
 
+
       this.client.write(`${JSON.stringify(authParams)}\r\n`);
 
       this.connectedAt = new Date().getTime();
+
+      this.messageSender = setInterval(this.sendUpdates.bind(this), 1000);
 
       this.client.on('data', (data) => {
 
@@ -63,34 +69,7 @@ class BetFairStreamAPI {
         // Parse the data String into JSON Object
         try {
           const result = JSON.parse(this.chunks.join(''));
-          console.log(result);
-
-          console.log(process.env.BETFAIR_CONNECTION_ERROR === 'test', !this.isTestDisconnected && now - this.connectedAt > testWaitTime, now - this.connectedAt);
-
-          // Connection status
-          if (result.op === 'status') {
-            if (result.connectionClosed) {
-              console.log('status with connection closed', result);
-              const { errorCode, errorMessage } = result;
-              this.socket.emit('subscription-error', { errorCode, errorMessage });
-            }
-            else {
-              for (let i = 0; i < this.subscriptions.length; i += 1) {
-                this.client.write(`${JSON.stringify(this.subscriptions[i])}\r\n`);
-              }
-            }
-            this.subscriptions = [];
-            this.connectionClosed = result.connectionClosed;
-          }
-
-          // Market Change Message Data Found
-          if (result.op === 'mcm' && result.mc) {
-            this.socket.emit('mcm', result);
-          }
-          // Order Change Message Data Found
-          else if (result.op === 'ocm' && result.oc) {
-            this.socket.emit('ocm', result);
-          }
+          this.updates.push(result);
           this.chunks = [];
         } catch (e) {
           //
@@ -107,12 +86,49 @@ class BetFairStreamAPI {
           this.socket.emit('subscription-error', { errorMessage: 'Disconnected from the market. Reconnect to start receiving market data.' });
           this.connectionClosed = true;
         }
+        this.messageSender = clearInterval(this.messageSender);
       });
 
       this.client.on('error', (data) => {
         console.log(`Error: ${data}`);
       });
     });
+  }
+
+  sendUpdates() {
+    try {
+      const result = this.updates.shift();
+      console.log(result);
+
+      if (result) {
+        // Connection status
+        if (result.op === 'status') {
+          if (result.connectionClosed) {
+            console.log('status with connection closed', result);
+            const { errorCode, errorMessage } = result;
+            this.socket.emit('subscription-error', { errorCode, errorMessage });
+          }
+          else {
+            for (let i = 0; i < this.subscriptions.length; i += 1) {
+              this.client.write(`${JSON.stringify(this.subscriptions[i])}\r\n`);
+            }
+          }
+          this.subscriptions = [];
+          this.connectionClosed = result.connectionClosed;
+        }
+
+        // Market Change Message Data Found
+        if (result.op === 'mcm' && result.mc) {
+          this.socket.emit('mcm', result);
+        }
+        // Order Change Message Data Found
+        else if (result.op === 'ocm' && result.oc) {
+          this.socket.emit('ocm', result);
+        }
+      }
+    } catch (error) {
+      //
+    }
   }
 
   subscribe(accessToken, params) {
@@ -148,7 +164,7 @@ class BetFairStreamAPI {
       },
       marketDataFilter: {
         fields: ['EX_ALL_OFFERS', 'EX_TRADED', 'EX_TRADED_VOL', 'EX_LTP', 'EX_MARKET_DEF'],
-      },  
+      },
     }
 
     if (initialClk) {
