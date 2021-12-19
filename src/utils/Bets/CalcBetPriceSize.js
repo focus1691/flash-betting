3386fd2f46739371fdeb784b5ee2475e7ae324e8
Joshua <mrjoshuaali@yahoo.co.uk>
2021-12-19 21:19:54 +0000

    Removed bet price size calculation

--- src/utils/Bets/CalcBetPriceSize.js
+++
@@ -1,8 +0,0 @@
-import { calcLiability } from '../TradingStategy/HedingCalculator';
-
-export default ((bet) => {
-  console.log('calcPriceSize()', bet.size, bet.price);
-  // bet.size = bet.side === 'LAY' ? calcLiability(bet.price, bet.size) : parseFloat(bet.size);
-  bet.price = parseFloat(bet.price);
-  return bet;
-});
