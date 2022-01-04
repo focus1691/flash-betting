fc29cc34a50c930daab9e5620c0cf57e8b8473af
Joshua <mrjoshuaali@yahoo.co.uk>
2022-01-04 02:38:56 +0000

    Fixed validation urls

--- server/utils/Validator.js
+++ server/utils/Validator.js
@@ -1,5 +1,5 @@
 function isAuthURL(url) {
-  return /(?:login|logout|get-subscription-status|get-vendor-client-id|keep-alive|registration-statusauthenticate-user|vendor-id|registration-status)/.test(url);
+  return /(?:login|logout|get-subscription-status|get-vendor-client-id|keep-alive|registration-status|authenticate-user|vendor-id|registration-status)/.test(url);
 }
 
 module.exports = {
