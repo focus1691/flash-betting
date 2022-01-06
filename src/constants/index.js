f86a70447e2701d6d2409a12cec81c470494da70
Joshua <mrjoshuaali@yahoo.co.uk>
2022-01-06 19:21:03 +0000

    Added betfair vendor login to consts

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,5 +4,6 @@ export const FIVE_SECONDS = 5000;
 export const TWO_HUNDRED_AND_FIFTY_MILLISECONDS = 250;
 export const FLASH_BETTING_URL = 'https://flash-betting.herokuapp.com/';
 export const FLASH_BETTING_PAYMENT_URL = 'http://localhost:3000/purchase';
+export const BETFAIR_LOGIN_PAGE = 'http://identitysso.betfair.com/view/vendor-login';
 
 export const SAFE_LADDER_LIMIT = 6;
