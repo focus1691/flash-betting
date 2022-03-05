e23c4df2d33f5b5a9151f389e17e79c13e9a738c
Joshua <mrjoshuaali@yahoo.co.uk>
2022-03-05 02:50:53 +0000

    removed non-auth errors from the AuthErrors list

---
+++ src/utils/Errors/GeneralErrors.js
@@ -0,0 +1,14 @@
+export default {
+  INVALID_INPUT_DATA: 'Invalid input data',
+  UNEXPECTED_ERROR: 'An unexpected internal error occurred that prevented successful request processing',
+  SERVICE_BUSY: 'The service is currently too busy to service this request',
+  TOO_MUCH_DATA: 'The operation requested too much data',
+  REQUEST_SIZE_EXCEEDS_LIMIT: 'The request exceeds the request size limit',
+  ACCESS_DENIED: 'The calling client is not permitted to perform the specific action',
+  TOO_MANY_REQUESTS: 'Too many requests',
+  INVALID_CLIENT_REF: 'Invalid length for the client reference',
+  WALLET_TRANSFER_ERROR: 'There was a problem transferring funds between your wallets',
+  DUPLICATE_CARDS: 'duplicate cards',
+  USER_SETTINGS_NOT_FOUND: "The User's Settings were not found",
+  MAX_CONNECTION_LIMIT_EXCEEDED: 'The User created more connections than allowed to',
+}
