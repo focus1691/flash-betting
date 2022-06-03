ef3b349cf41e6e42f8899e90a80cc7f26df18ae0
Joshua <mrjoshuaali@yahoo.co.uk>
2022-06-03 09:26:21 +0100

    updated hover style

--- src/jss/components/Sidebar/toolbarStyle.js
+++ src/jss/components/Sidebar/toolbarStyle.js
@@ -1,6 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(0.5),
+  },
   toolbar: {
     borderRadius: theme.spacing(2),
     backgroundColor: '#242526',
@@ -9,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'wrap',
     alignItems: 'stretch',
     '& button': {
-      flexBasis: 'calc(100% / 5)',
+      flexBasis: 'calc(100% / 4)',
       width: '15px',
       border: '0',
       background: 'transparent',
@@ -19,9 +25,12 @@ const useStyles = makeStyles((theme) => ({
       },
       '& img': {
         width: '32px',
-        height: '32px',
+        height: '100%',
       },
-      '&:nth-child(6) > img': {
+      '&:nth-child(6)': {
+        cursor: 'default',
+      },
+      '&:nth-child(6) img': {
         cursor: 'not-allowed',
       },
     },
